0c32a1cfd9bd4f625f89beb6999e34f6a78d41fa
PV <viguiep@gmail.com>
2021-01-27 13:33:26 +0100

    upload & delete implemented

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ https://github.com/hayanisaid/bootstrap4-website
 
 Reste
 https://getbootstrap.com/docs/5.0/examples/album/
+https://getbootstrap.com/docs/4.0/examples/checkout/
+https://getbootstrap.com/docs/4.0/examples/sign-in/
+https://getbootstrap.com/docs/4.0/examples/dashboard/
 
 Favicon
 https://favicon.io/favicon-converter/
@@ -13,3 +16,50 @@ https://www.w3schools.com/colors/colors_hexadecimal.asp
 
 Make PNG background transparent
 https://onlinepngtools.com/create-transparent-png
+
+Python file management (uploading & processing files)
+https://viveksb007.wordpress.com/2018/04/07/uploading-processing-and-downloading-files-in-flask/
+https://www3.ntu.edu.sg/home/ehchua/programming/webprogramming/Python_FileText.html
+
+FLASK PROJECT FILE STRUCTURE (+ tutorial)
+https://pythonbasics.org/flask-boilerplate/
+
+FLASK EXTENSIONS
+https://www.fullstackpython.com/flask-extensions-plug-ins-related-libraries.html
+https://nickjanetakis.com/blog/15-useful-flask-extensions-and-libraries-that-i-use-in-every-project
+gunicorn
+Flask-Login, Flask-WTF & WTForms-Components, Flask-SQLAlchemy & psycopg2
+flask-debugtoolbar, pytest & pyest-cov, flake8 => code debugging, testing & purity
+alembic, Flask-Migrate => database migration
+celery & redis => background jobs
+Flask-Limiter => control traffic
+Flask-Mail
+Flask App Builder => generated apps that include default security settings, forms, and internationalization support
+Flask Sijax-Sijax => to make AJAX easy to use in web apps
+Flask-Admin
+Flask-Uploads
+
+Flask-Marshmallow, Flask-Cache, flask-jwt, flask-jwt-extended, flask-toastr, connexion, flask-nav, flask-blueprints
+flask-base, flask-bootstrap, Flask-Meld, flask-praetorian, Flask RESTX, Flask-Security-Too, Flask-User, Flask-VueJs-Template
+flask-mongoengine, Flask-Redis
+
+
+PDF (top)
+https://dzone.com/articles/exporting-data-from-pdfs-with-python
+https://github.com/jsvine/pdfplumber (explanations)
+https://euske.github.io/pdfminer/
+
+POSSIBLE DATASET
+https://github.com/BeelGroup/GIANT-The-1-Billion-Annotated-Synthetic-Bibliographic-Reference-String-Dataset
+
+PARSING => search "python doi parsing"
+https://github.com/glooney/python-bibref-parser/blob/master/bibref_parser/parser.py
+https://www.crossref.org/labs/resolving-citations-we-dont-need-no-stinkin-parser/
+
+https://github.com/titipata/pubmed_parser
+https://github.com/fabiobatalha/crossrefapi
+
+Articles:
+https://link.springer.com/chapter/10.1007/978-3-642-33290-6_40 (Improved Bibliographic Reference Parsing Based on Repeated Patterns)
+
+INTERESTING: https://pubmed.ncbi.nlm.nih.gov/32044951/
--- app/views.py
+++ app/views.py
@@ -1,21 +1,107 @@
 # views.py
 
-from flask import render_template
+from flask import render_template, redirect, url_for, request
+from werkzeug.utils import secure_filename
 
 from app import app
 
-TEAM = [('Brais', 'Crispr/CAS 9 scientist', 'brais.jpg'),
-        ('Yew Mun', 'Bioinformatics researcher', 'yewmun.jpg'),
-        ('Pascal', 'Serial entrepreneur', 'pascal.jpg')]
+"""
+@app.route('/')
+@app.route('/dashboard')
+@app.route('/signin')
+@app.route('/upload')
+@app.route('/register')
+@app.route("/album")
+@app.route('/album_view/<article_name>')
+
+A FAIRE:
+- upload article pdf (avec contrôle type fichier + taille + nombre de fichier totaux (max 10?)
+- delete article (avec contrôle des droits)
+- user management
+- dashboard/admin management
+- user profile
+"""
+
+ALLOWED_EXTENSIONS = {'pdf'}
+
+def allowed_file(filename):
+    """
+    Checks the filename for allowed file extension.
+    If file type is supported the function returns True otherwise it returns False.
+    """
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 import os
 import PyPDF2
 DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = DIR_PATH + '/uploads/'
 
+from werkzeug.exceptions import RequestEntityTooLarge
+
+@app.errorhandler(413)
+@app.errorhandler(RequestEntityTooLarge)
+def app_handle_413(e):
+    return 'File Too Large', 413
+
+
+
 @app.route('/')
 def index():
+    TEAM = [('Brais', 'Crispr/CAS 9 scientist', 'brais.jpg'),
+            ('Yew Mun', 'Bioinformatics researcher', 'yewmun.jpg'),
+            ('Pascal', 'Serial entrepreneur', 'pascal.jpg')]
     return render_template("index.html", team=TEAM)
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    """
+    cf. https://viveksb007.wordpress.com/2018/04/07/uploading-processing-and-downloading-files-in-flask/
+    """
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print('No file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(os.path.join(UPLOAD_FOLDER, filename))
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect(url_for('album'))
+    max_size_Mo = int(app.config['MAX_CONTENT_LENGTH'] / 1024 / 1024)
+    return render_template("upload.html", max_size_Mo = max_size_Mo)
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/dashboard')
+def dashboard():
+    DATA = [[1001, 'Lorem', 'ipsum', 'dolor', 'sit'],
+    [1002, 'amet', 'consectetur', 'adipiscing', 'elit'],
+    [1003, 'Integer', 'nec', 'odio', 'Praesent'],
+    [1003, 'libero', 'Sed', 'cursus', 'ante'],
+    [1004, 'dapibus', 'diam', 'Sed', 'nisi'],
+    [1005, 'Nulla', 'quis', 'sem', 'at'],
+    [1006, 'nibh', 'elementum', 'imperdiet', 'Duis'],
+    [1007, 'sagittis', 'ipsum', 'Praesent', 'mauris'],
+    [1008, 'Fusce', 'nec', 'tellus', 'sed'],
+    [1009, 'augue', 'semper', 'porta', 'Mauris'],
+    [1010, 'massa', 'Vestibulum', 'lacinia', 'arcu'],
+    [1011, 'eget', 'nulla', 'Class', 'aptent'],
+    [1012, 'taciti', 'sociosqu', 'ad', 'litora'],
+    [1013, 'torquent', 'per', 'conubia', 'nostra'],
+    [1014, 'per', 'inceptos', 'himenaeos', 'Curabitur'],
+    [1015, 'sodales', 'ligula', 'in', 'libero'],
+    ]
+    return render_template("dashboard.html", data=DATA)
 
 @app.route("/album")
 def album():
@@ -25,9 +111,15 @@ def album():
 
 
 @app.route('/album_view/<article_name>')
-def affiche_article(article_name):
+def show_article(article_name):
     with open(UPLOAD_FOLDER + article_name, 'rb') as myPdf:
         pdfReader = PyPDF2.PdfFileReader(myPdf)
         page_one = pdfReader.getPage(0)
         text = page_one.extractText()
     return render_template('album_view.html', article_name = article_name, article_text = text[:1500])
+
+@app.route('/album_delete/<article_name>')
+def delete_article(article_name):
+    if os.path.isfile(UPLOAD_FOLDER + article_name):
+        os.remove(UPLOAD_FOLDER + article_name)
+    return render_template('album_delete.html', article_name = article_name)
